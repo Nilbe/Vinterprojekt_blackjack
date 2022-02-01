1c2646d5fb25e9545228e40bfabe9a7791842fed
Nils Berglund <nilsberglund04@gmail.com>
2022-02-01 11:02:12 +0100

    började på den andra delen spelet

--- Vinterprojekt_blackjack/Program.cs
+++ Vinterprojekt_blackjack/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 
-
 Console.WriteLine("Welcome to blackjack, press enter to start");
 Console.ReadLine();
 
@@ -13,27 +12,53 @@ static void bet()
     int maxBet = money;
     int minBet = 1;
 
+    int bet = 0;
     bool success = false;
-    int outcome;
 
     while(success != true)
     {
         Console.WriteLine("Your money: " + money + " How much would you like to bet?");
-        string bet = Console.ReadLine();
+        string input = Console.ReadLine();
 
-        success = int.TryParse(bet, out outcome);
+        success = int.TryParse(input, out bet);
     }
-
+    
     if(success == true)
     {
-        if(outcome > maxBet || outcome < minBet)
+        if(bet > maxBet || bet < minBet)
         {
             Console.WriteLine("You need to bet a amout of money that you have");
         }
-        else
+        else if(bet <= maxBet || bet >= minBet)
         {
-            Console.WriteLine("You have bet " + outcome + " moneys");
+            Console.WriteLine("You have bet " + bet + " moneys");
         }
+    }
+}
+
+
+deal();
+
+static void deal()
+{
+    Random generator = new Random();
+
+    Console.WriteLine("Press enter to begin");
+    Console.ReadLine();
+
+    int yourCards = 0;
+    int dealersCards = 0;
+    
+    while(yourCards < 21 && dealersCards < 21)
+    {
+        yourCards = generator.Next(2,11);
+        dealersCards = generator.Next(2,11);
+
+        
+        Console.WriteLine("Your cards total " + yourCards + ". Would you like to stand (s) or draw another (h)");
+        string hitOrStand = Console.ReadLine();
+        
+
     }
 }
 
